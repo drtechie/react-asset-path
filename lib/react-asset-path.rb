e9d949c242afcd6f43820cfa7bd2f190b441db43
Mithun James <drmithunjames@gmail.com>
2017-10-17 15:35:08 +0530

    Add new env, get only images

--- lib/react-asset-path.rb
+++ lib/react-asset-path.rb
@@ -1,35 +1,35 @@
 module ReactAssetPath
   VERSION   = '0.0.1'
-  IMAGE_EXT = %w{.jpg .jpeg .png .ico .svg}
 
   class Engine < ::Rails::Engine
   end
 
   def self.perform?
-    Rails.env.production? || Rails.env.staging?
+    Rails.env.production? || Rails.env.staging? || Rails.env.dev?
   end
 
   def self.mapping
     data   = {}
-    prefix = Rails.application.config.assets.prefix
+    asset_host = Rails.application.config.asset_host
+    image_files = File.join("**", "*.{jpg, jpeg, png, ico, svg}")
 
     images_path = Rails.root.join('app/assets/images/')
     images_dir  = images_path.to_s
+    image_files_in_dir = File.join(images_dir, image_files)
 
-    Dir["#{images_dir}**/*.*"].each do |file_path|
+    Dir.glob(image_files_in_dir).each do |file_path|
       basename = File.basename(file_path)
 
       Rails.logger.info file_path
 
-      if IMAGE_EXT.include?(File.extname(basename))
-        if perform?
-          file_path = ActionController::Base.helpers.asset_path(basename)
-        else
-          file_path = "/assets/#{basename}"
-        end
-
-        data[basename] = file_path
+      if perform?
+        file_path = ActionController::Base.helpers.asset_path(basename)
+      else
+        file_path = "/assets/#{basename}"
       end
+
+      data[basename] = asset_host ? asset_host + file_path : file_path
+
     end
 
     data
